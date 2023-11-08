b2815726ff789854ee9f248bdaaa42311acc2c45
AE/BHH <ehsaniabbas9@gmail.com>
2023-11-08 09:32:00 -0700

    useEffect hook, custom hook and fragment practiced in week 5 assignment

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import TodoList from './TodoList'
 import AddTodoForm from './AddTodoForm'
 
+function useSemiPersistentState(key, initialState) {
+	const savedValue = localStorage.getItem(key) || initialState
+	console.log(savedValue)
+
+	const [value, setValue] = useState(JSON.parse(savedValue))
+
+	useEffect(() => {
+		localStorage.setItem(key, JSON.stringify(value))
+	}, [key, value])
+	return [value, setValue]
+}
 function App() {
-	const [todoList, setTodoList] = React.useState([])
+	const [todoList, setTodoList] = useSemiPersistentState('savedTodoList', [])
 
 	function addTodo(newTodo) {
 		setTodoList([...todoList, newTodo])
