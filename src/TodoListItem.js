2c4e823f1c9b382e19ea126f96cfa5aebff770f6
AE/BHH <ehsaniabbas9@gmail.com>
2024-01-24 06:21:31 -0700

    CSS added for styling the todo list project

--- src/TodoListItem.js
+++ src/TodoListItem.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import style from './TodoListItem.module.css'
 
 function TodoListItem({ listItem, onRemoveTodo }) {
 	console.log(listItem)
 	return (
-		<li>
+		<li className={style.ListItem}>
 			{listItem.title}
-			<button type='button' onClick={() => onRemoveTodo(listItem.id)}>
+			<button className={`btn ${style.btn}`} type='button' onClick={() => onRemoveTodo(listItem.id)}>
 				Remove
 			</button>
 		</li>
